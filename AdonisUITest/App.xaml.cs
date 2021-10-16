974cf3dbe1a577b0e2643d389dd4b9a52cab816a
kdemuth <kdemuth@gmail.com>
2021-10-16 01:27:29 -0400

    Add files via upload

---
+++ AdonisUITest/App.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace AdonisUITest
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+            DispatcherUnhandledException += App_DispatcherUnhandledException;
+            TaskScheduler.UnobservedTaskException += TaskScheduler_UnobservedTaskException;
+
+            base.OnStartup(e);
+        }
+
+        private void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            // Process unhandled exception
+            Debug.WriteLine($"{nameof(CurrentDomain_UnhandledException)}: {e.ExceptionObject}");
+            MessageBox.Show($"{nameof(CurrentDomain_UnhandledException)}: {(e.ExceptionObject as Exception)?.Message}");
+        }
+
+        private void App_DispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            // Process unhandled exception
+            Debug.WriteLine($"{nameof(App_DispatcherUnhandledException)}: {e.Exception}");
+            MessageBox.Show($"{nameof(App_DispatcherUnhandledException)}: {e.Exception.Message}");
+
+            // Prevent default unhandled exception processing
+            e.Handled = true;
+        }
+
+        private void TaskScheduler_UnobservedTaskException(object sender, UnobservedTaskExceptionEventArgs e)
+        {
+            // Process unhandled exception
+            Debug.WriteLine($"{nameof(TaskScheduler_UnobservedTaskException)}: {e.Exception}");
+            MessageBox.Show($"{nameof(TaskScheduler_UnobservedTaskException)}: {e.Exception.Message}");
+
+            // Prevent default unhandled exception processing
+            e.SetObserved();
+        }
+    }
+}
---
+++ AdonisUITest/MainWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Input;
+
+using AdonisUI;
+
+namespace AdonisUITest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private const bool UseItemSource = false;
+
+        private ObservableCollection<DiagnosticsItem> m_ItemCollection = new();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private bool m_Loaded;
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (m_Loaded) { return; }
+            m_Loaded = true;
+
+            if (UseItemSource)
+            {
+                TestDataGrid.ItemsSource = m_ItemCollection;
+            }
+        }
+
+        private void AddDataGridItemButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            DiagnosticsItem item = new() { Timestamp = DateTime.Now.ToString("MM/dd/yyyy HH:mm:ss.fff"), Data = "Test record" };
+
+            if (UseItemSource)
+            {
+                m_ItemCollection.Add(item);
+                while (m_ItemCollection.Count > 100)
+                {
+                    m_ItemCollection.RemoveAt(0);
+                }
+            }
+            else
+            {
+                TestDataGrid.Items.Add(item);
+                while (TestDataGrid.Items.Count > 100)
+                {
+                    TestDataGrid.Items.RemoveAt(0);
+                }
+            }
+
+            TestDataGrid.ScrollIntoView(item);
+        }
+
+        private void Window_PreviewMouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            ToggleTheme();
+        }
+
+        private bool _darkMode;
+
+        private void ToggleTheme()
+        {
+            ResourceLocator.SetColorScheme(Application.Current.Resources, _darkMode ? ResourceLocator.LightColorScheme : ResourceLocator.DarkColorScheme);
+
+            _darkMode = !_darkMode;
+        }
+    }
+
+    class DiagnosticsItem
+    {
+        public string Timestamp { get; set; }
+        public string Data { get; set; }
+    }
+}
